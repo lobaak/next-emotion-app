c03dffb9ebd068ead587bb93a832ad17c2ea886b
Nathan Thorpe <nathan.thorpe@tal.com.au>
2025-11-27 10:38:42 +1100

    Add theme type

---
+++ app/theme.d.ts
@@ -0,0 +1,19 @@
+import "@emotion/react";
+
+import { Theme as ThemeType } from "@tal-gel/theming-plugin-tal2.0";
+
+// Export all components
+export * from "./index";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    fonts: ThemeType["fonts"];
+    breakpoints: ThemeType["breakpoints"];
+    getSize: ThemeType["getSize"];
+    icons: ThemeType["icons"];
+    id: ThemeType["id"];
+    tokens: ThemeType["tokens"];
+    bodyFont?: ThemeType extends { bodyFont: infer T } ? T : undefined;
+    [key: string]: unknown;
+  }
+}
